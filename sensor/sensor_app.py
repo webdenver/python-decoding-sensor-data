05bf8a9fa2a2c4b40f97ef33023bccf480c3c2a9
webdenver <>
2021-03-13 13:42:10 +0500

    module1

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,3 +1,4 @@
+from load_data import load_sensor_data
 # Runner script for all modules
 
 
@@ -6,6 +7,8 @@
 # They are needed to validate your unittest
 data = []
 print("Sensor Data App")
+data = load_sensor_data()
+print("Loaded records: {}".format(len(data)))
 ##############################
 
 # Module 1 code here:
