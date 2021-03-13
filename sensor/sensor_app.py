726c58d857774194c4d62afb9db921b254ac92dc
webdenver <>
2021-03-13 14:02:45 +0500

    module2

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,4 +1,6 @@
 from load_data import load_sensor_data
+from house_info import HouseInfo
+from datetime import date, datetime
 # Runner script for all modules
 
 
@@ -7,14 +9,20 @@ from load_data import load_sensor_data
 # They are needed to validate your unittest
 data = []
 print("Sensor Data App")
-data = load_sensor_data()
-print("Loaded records: {}".format(len(data)))
 ##############################
 
 # Module 1 code here:
-
+data = load_sensor_data()
+print("Loaded records: {}".format(len(data)))
 # Module 2 code here:
+house_info = HouseInfo(data)
+test_area = 1
+recs = house_info.get_data_by_area("id", rec_area=test_area)
+print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
 
+test_date = datetime.strptime("5/9/20", "%m/%d/%y")
+recs = house_info.get_data_by_date("id", rec_date=test_date)
+print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 # Module 3 code here:
 
 # Module 4 code here:
